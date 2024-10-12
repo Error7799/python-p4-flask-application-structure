8c983ae063a645024a046705ce5182a58c370015
Error7799 <kingy7799sentamu@gmail.com>
2024-10-12 23:26:41 +0300

    Fun

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    return "<h1>Home Page</h1>"
+
+@app.route("/<urs>")
+def User(urs):
+    return f"<h1>Hello {urs}</h1>"
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
